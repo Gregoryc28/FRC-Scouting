eca2a1cbce2f2a052a13d32678d71ac115d54339
Gregory Cohen <71674088+Gregoryc28@users.noreply.github.com>
2025-03-13 19:27:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,36 +24,36 @@ from data import *
 # Google Analytics
 
 
-def inject_ga():
-    GA_ID = "google_analytics"
+# def inject_ga():
+#     GA_ID = "google_analytics"
 
-    GA_JS = """
-   <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NH20RWNP3G"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-NH20RWNP3G');
-</script>
-    """
+#     GA_JS = """
+#    <!-- Google tag (gtag.js) -->
+# <script async src="https://www.googletagmanager.com/gtag/js?id=G-NH20RWNP3G"></script>
+# <script>
+#   window.dataLayer = window.dataLayer || [];
+#   function gtag(){dataLayer.push(arguments);}
+#   gtag('js', new Date());
+#   gtag('config', 'G-NH20RWNP3G');
+# </script>
+#     """
 
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+#     # Insert the script in the head tag of the static template inside your virtual
+#     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+#     logging.info(f'editing {index_path}')
+#     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+#     if not soup.find(id=GA_ID):
+#         bck_index = index_path.with_suffix('.bck')
+#         if bck_index.exists():
+#             shutil.copy(bck_index, index_path)
+#         else:
+#             shutil.copy(index_path, bck_index)
+#         html = str(soup)
+#         new_html = html.replace('<head>', '<head>\n' + GA_JS)
+#         index_path.write_text(new_html)
 
 
-inject_ga()
+# inject_ga()
 
 #team = 564
 teams = [564, 870, 263, 514, 527, 694]
